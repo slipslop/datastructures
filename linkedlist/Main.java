76c91f33535732ef9a4aee78fc4b6ce4b0875e4c
slipslop <vili.takaanttila@gmail.com>
2025-02-21 08:16:27 +0200

    Update Main

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -3,10 +3,14 @@ public class Main {
         LinkedList a = new LinkedList();
         a.insert(10);
         a.insert(20);
-        a.insert(5);
+        a.insert(20);
+        a.insert(21);
         a.insert(22);
+        a.insert(23);
+        a.insert(5);
 
-        Node n = a.search(10);
-        System.out.println(n.getNext());
+        Node n = a.search(20);
+        a.delete(n);
+        System.out.println(a.toString());
     }
 }
